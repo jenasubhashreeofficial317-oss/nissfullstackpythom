224289a8051ae9a783cb174bba61701183e2656b
subhashree jena <jenasubhashreeofficial317@gmail.com>
2026-02-22 22:15:20 +0530

    demo13test

---
+++ python niis/basic/demo13.py
@@ -0,0 +1,9 @@
+"""wap to calculate simple interst"""
+print("enter principal")
+p=float(input())
+print("enter rate")
+R=float(input())
+print("enter time")
+T=float(input())
+SI=(P*R*T)/100
+print("simple intrest=",SI)
