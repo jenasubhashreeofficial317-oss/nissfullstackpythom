4251dcbe5bd4ef89ed823940249e8aeb6d2572f3
subhashree jena <jenasubhashreeofficial317@gmail.com>
2026-02-27 00:49:33 +0530

    functiontest

---
+++ python niis/function/func2.py
@@ -0,0 +1,6 @@
+#function check even number
+def check_even(num):
+ if num%2==0:
+   print(num,"is even number")
+number=int(input())
+check_even(number)
---
+++ python niis/function/func3.py
@@ -0,0 +1,6 @@
+#function check odd number
+def check_odd(num):
+  if num%2!=0:
+  	print("enter odd number")
+number=int(input())
+check_odd(number)
