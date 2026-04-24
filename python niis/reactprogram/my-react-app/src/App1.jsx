11d0c649458b59d65766190cafb796e8e86e6c6a
subhashree jena <jenasubhashreeofficial317@gmail.com>
2026-04-24 21:33:32 +0530

    javascriptprogram

---
+++ python niis/reactprogram/my-react-app/src/App1.jsx
@@ -0,0 +1,16 @@
+function App1() {
+
+  // 🔸 Creating JSX
+  const element = <h1>Hello JSX!</h1>
+
+  return (
+    <div>
+      
+      {element}
+
+      <p>This is a paragraph</p>
+    </div>
+  )
+}
+
+export default App1
